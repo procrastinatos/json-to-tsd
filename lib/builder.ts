dc18783d3ad256bf91183d69990fdc2f00cab7a9
procrastinatos <robby.cornelissen@gmail.com>
2016-02-27 20:07:42 +0900

    refactored model to be class-based

--- lib/builder.ts
+++ lib/builder.ts
@@ -1,9 +1,29 @@
 'use strict';
 
-import {StringSchema} from "./model";
+import {Schema, StringSchema, NumberSchema, IntegerSchema, BooleanSchema, ArraySchema, ObjectSchema, NullSchema} from "./model";
 
 export class Builder {
-    buildFromJsonSchema(jsonSchema: any): void {
+    private static SCHEMA_TYPES: { [name: string]: typeof Schema } = {
+        'string': StringSchema,
+        'number': NumberSchema,
+        'integer': IntegerSchema,
+        'boolean': BooleanSchema,
+        'array': ArraySchema,
+        'object': ObjectSchema,
+        'null': NullSchema
+    };
 
+    build(jsonSchema: any): void {
+        if (!jsonSchema) {
+            throw new Error('No schema provided');
+        }
+
+        if (!jsonSchema.type) {
+            throw new Error('Type inference not implemented');
+        }
+
+        if (!Builder.SCHEMA_TYPES[jsonSchema.type]) {
+            throw new Error('Unsupported type ' + jsonSchema.type);
+        }
     }
 }
--- lib/model.ts
+++ lib/model.ts
@@ -2,53 +2,53 @@
 
 type Type = 'string' | 'number' | 'integer' | 'boolean' | 'array' | 'object' | 'null';
 
-export interface Schema {
-    id?: string; // format: uri
-    $schema?: string; // format: uri
-    type?: Type | Type[]; // if array: minItems: 1, uniqueItems: true
-    title?: string;
-    description?: string;
-    default?: any; // should validate against the schema in which it resides, but that isn’t required
-    enum?: any[]; // minItems: 1, uniqueItems: true, should also be valid against the enclosing schema
-    anyOf?: Schema[]; // schemaArray
-    allOf?: Schema[]; // schemaArray
-    oneOf?: Schema[]; // schemaArray
-    not?: Schema;
-    definitions?: { [name: string]: Schema };
+export abstract class Schema {
+    id: string; // format: uri
+    $schema: string; // format: uri
+    type: Type | Type[]; // if array: minItems: 1, uniqueItems: true
+    title: string;
+    description: string;
+    default: any; // should validate against the schema in which it resides, but that isn’t required
+    enum: any[]; // minItems: 1, uniqueItems: true, should also be valid against the enclosing schema
+    anyOf: Schema[]; // schemaArray
+    allOf: Schema[]; // schemaArray
+    oneOf: Schema[]; // schemaArray
+    not: Schema;
+    definitions: { [name: string]: Schema };
 }
 
-export interface StringSchema extends Schema {
-    minLength?: number; // positiveIntegerDefault0
-    maxLength?: number; // positiveInteger
-    pattern?: string; // format: regex
-    format?: string; // date-time, email, hostname, ipv4, ipv6, uri
+export class StringSchema extends Schema {
+    minLength: number; // positiveIntegerDefault0
+    maxLength: number; // positiveInteger
+    pattern: string; // format: regex
+    format: string; // date-time, email, hostname, ipv4, ipv6, uri
 }
 
-interface NumericalSchema extends Schema {
-    multipleOf?: number; // minimum: 0, exclusiveMinimum: true
-    minimum?: number;
-    maximum?: number;
-    exclusiveMinimum?: boolean; // default false
-    exclusiveMaximum?: boolean; // default false
+abstract class NumericalSchema extends Schema {
+    multipleOf: number; // minimum: 0, exclusiveMinimum: true
+    minimum: number;
+    maximum: number;
+    exclusiveMinimum: boolean; // default false
+    exclusiveMaximum: boolean; // default false
 }
 
-export interface NumberSchema extends NumericalSchema {
+export class NumberSchema extends NumericalSchema {
 }
 
-export interface IntegerSchema extends NumericalSchema {
+export class IntegerSchema extends NumericalSchema {
 }
 
-export interface ObjectSchema extends Schema {
-    properties?: { [key: string]: Schema }; // default {}
-    additionalProperties?: boolean | Schema; // default {}
-    patternProperties?: { [regex: string]: Schema } // default {}
-    required?: string[]; // minItems: 1, uniqueItems: true
-    minProperties?: number; // positiveIntegerDefault0
-    maxProperties?: number; // positiveInteger
-    dependencies?: string[] | Schema; // stringArray or schema
+export class ObjectSchema extends Schema {
+    properties: { [key: string]: Schema }; // default {}
+    additionalProperties: boolean | Schema; // default {}
+    patternProperties: { [regex: string]: Schema } // default {}
+    required: string[]; // minItems: 1, uniqueItems: true
+    minProperties: number; // positiveIntegerDefault0
+    maxProperties: number; // positiveInteger
+    dependencies: string[] | Schema; // stringArray or schema
 }
 
-export interface ArraySchema extends Schema {
+export class ArraySchema extends Schema {
     items: Schema | Schema[]; // schema or schemaArray
     additionalItems: boolean | Schema; // default {}
     minItems: number; // positiveIntegerDefault0
@@ -56,8 +56,8 @@ export interface ArraySchema extends Schema {
     uniqueItems: boolean; // default false
 }
 
-export interface BooleanSchema extends Schema {
+export class BooleanSchema extends Schema {
 }
 
-export interface NullSchema extends Schema {
+export class NullSchema extends Schema {
 }
